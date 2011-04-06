f9e08f91ce11587e91db033ae652c8e4a6b16e4d
Clive Crous <clive@crous.co.za>
2011-04-06 17:00:56 +0200

    Add yesterday, this_month_so_far and last_month

--- lib/adsense-info.rb
+++ lib/adsense-info.rb
@@ -21,9 +21,33 @@ module Adsense
       nil
     end
 
-    def today_so_far
+    def update_values!
       login!
-      ( get('https://www.google.com/adsense/v3/m/home') / 'span[@class~=value]' ).first.inner_html
+      values = ( get('https://www.google.com/adsense/v3/m/home') / 'span[@class~=value]' )
+      @today_so_far = values[0].inner_html
+      @yesterday = values[1].inner_html
+      @this_month_so_far = values[2].inner_html
+      @last_month = values[3].inner_html
+    end
+
+    def today_so_far
+      update_values! unless @today_so_far
+      @today_so_far
+    end
+
+    def yesterday
+      update_values! unless @yesterday
+      @yesterday
+    end
+
+    def this_month_so_far
+      update_values! unless @this_month_so_far
+      @this_month_so_far
+    end
+
+    def last_month
+      update_values! unless @last_month
+      @last_month
     end
 
     private
